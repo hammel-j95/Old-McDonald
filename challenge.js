3948c40f91a6e488ded4ecbc4f86cfea4e67b5f4
hammel-j95 <hammelj95@gmail.com>
2015-02-02 10:47:22 -0500

    here a moo
    :cow:

--- challenge.js
+++ challenge.js
@@ -1,9 +1,10 @@
 document.writeln("Old McDonald had a farm, eyi eyi oh!")
 var animal = ["cow", "chicken", "turkey", "kangaroo", "t-rex", "cat"]
+var noise = ["moo-moo", "bawk-bawk", "gobble-gobble", "g'day mate-g'day mate", "rawr-rawr", "meow-meow"]
 for (var i = 0; i < animal.length; i++) {
   document.writeln("And on his farm he had a " + animal [i] + ", eyi eyi oh!");
-  if (animal[i]=== "cow"){
-    document.writeln("With a moo-moo here, and a moo moo there");
+  for (var i = 0; i < noise.length; i++){
+    document.writeln("With a" + [i] + "here and a" + [i] + "there");
   }
   document.writeln("Old McDonald had a farm, eyi eyi oh!")
 }
