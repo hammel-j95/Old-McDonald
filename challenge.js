9f606bd5f8254fa8fdb41b8f0aa78293073c03b4
hammel-j95 <hammelj95@gmail.com>
2015-02-02 10:36:26 -0500

    Old McDonald Challenge
    :cow:

---
+++ challenge.js
@@ -0,0 +1,9 @@
+document.writeln("Old McDonald had a farm, eyi eyi oh!")
+var animal = ["cow", "chicken", "turkey", "kangaroo", "t-rex", "cat"]
+for (var i = 0; i < animal.length; i++) {
+  document.writeln("And on his farm he had a " + animal [i] + ", eyi eyi oh!");
+  if (animal[i]=== "cow"){
+    document.writeln("With a moo-moo here, and a moo moo there");
+  }
+  document.writeln("Old McDonald had a farm, eyi eyi oh!")
+}
